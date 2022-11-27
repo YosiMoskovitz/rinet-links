d561052487975b73e18db16b3f0ad1f676de5c14
Yosi Moskovitz <05276ym@gmail.com>
2022-11-27 02:46:16 +0200

    change back server address

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,5 +1,5 @@
 export const APIconfig = {
-  url: 'https://rinet-links.herokuapp.com',
+  url: 'http://75.119.133.33:3001',
   reCaptchaSiteKey: '6LfRFDgeAAAAAN_nkOWyrqCkSqQkGTWd1RNRhL1Z',
   nedarimApiKey: 'j+iyEFN3bE',
   nedarimMOsadKey: '7003037'
