c7cea0ef2c162934a4804caef4b243c376c00b49
Yosi Moskovitz <05276ym@gmail.com>
2022-11-28 12:22:46 +0200

    change servers side url

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,5 +1,5 @@
 export const APIconfig = {
-  url: 'http://75.119.133.33:3001',
+  url: 'https://rinet-links-server.vercel.app',
   reCaptchaSiteKey: '6LfRFDgeAAAAAN_nkOWyrqCkSqQkGTWd1RNRhL1Z',
   nedarimApiKey: 'j+iyEFN3bE',
   nedarimMOsadKey: '7003037'
